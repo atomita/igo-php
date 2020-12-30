bd854467b13912447b60ecc25d0dba86635851ad
atomita <sleeping.cait.sith+gh@gmail.com>
2020-12-30 19:03:03 +0900

    Add phpunit

---
+++ tests/MorphemeTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tests;
+
+use atomita\Igo;
+use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertThat;
+use function PHPUnit\Framework\isJson;
+
+class MorphemeTest extends TestCase
+{
+    /**
+     * @testWith ["foo","bar",1]
+     */
+    public function testCanGetProperty($surface, $feature, $start)
+    {
+        $morpheme = new Igo\Morpheme($surface, $feature, $start);
+
+        assertEquals($surface, $morpheme->surface);
+        assertEquals($feature, $morpheme->feature);
+        assertEquals($start, $morpheme->start);
+
+        assertThat((string) $morpheme, isJson());
+    }
+}
